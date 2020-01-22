1676687cf6fb88f8fa705a54f9b389131d8deccf
Newton Xu <newton.xu@hotmail.com>
2020-01-21 19:46:23 -0500

    First commit'

---
+++ src/contest1.cpp
@@ -0,0 +1,107 @@
+#include <ros/console.h>
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+#include <kobuki_msgs/BumperEvent.h>
+#include <sensor_msgs/LaserScan.h>
+
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+
+float posX=0.0, posY=0.0, yaw=0.0;
+uint8_t bumper[3] = {kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED, kobuki_msgs::BumperEvent::RELEASED};
+#define N_BUMPER (3)
+#define RAD2DEG(rad) ((rad)*180./M_PI)
+#define DEG2RAD(deg) ((deg)*M_PI/180.)
+
+#include <stdio.h>
+#include <cmath>
+
+#include <chrono>
+using namespace std;
+
+void bumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg)
+{
+	//fill with your code
+
+       //std::cout<< "State=" << msg->state << endl;
+       //std::cout<< "Bumper=" << msg->bumper << endl;
+    bumper[msg->bumper] = msg->state;
+}
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+	//fill with your code
+}
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
+{
+    posX = msg->pose.pose.position.x;
+    posY = msg->pose.pose.position.y;
+    yaw = tf::getYaw(msg->pose.pose.orientation);
+    tf::getYaw(msg->pose.pose.orientation);
+    ROS_INFO("Position:(%f, %f) Orientation: %f rad or %f degrees.", posX,posY, yaw, RAD2DEG(yaw));
+
+}
+
+int main(int argc, char **argv)
+{
+    std::cout <<"hello, super kms" <<std::endl;
+    ros::init(argc, argv, "image_listener");
+    ros::NodeHandle nh;
+
+    ros::Subscriber bumper_sub = nh.subscribe("mobile_base/events/bumper", 10, &bumperCallback);
+    ros::Subscriber laser_sub = nh.subscribe("scan", 10, &laserCallback);
+    ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback);
+
+    ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
+
+    ros::Rate loop_rate(10);
+    std::cout <<"hello, kms" <<std::endl;
+
+    geometry_msgs::Twist vel;
+
+    // contest count down timer
+    std::chrono::time_point<std::chrono::system_clock> start;
+    start = std::chrono::system_clock::now();
+    uint64_t secondsElapsed = 0;
+
+    float angular = 0.0;
+    float linear = 0.0;
+
+    while(ros::ok() && secondsElapsed <= 480) {
+        ros::spinOnce();
+        //fill with your code
+        /*
+        bool any_bumper_pressed = false;
+        for (uint32_t b_idx = 0; b_idx < N_BUMPER; ++b_idx) {
+            any_bumper_pressed |= (bumper[b_idx] == kobuki_msgs::BumperEvent::PRESSED);
+        }
+        //
+        // Control logic after bumpers are being pressed.
+        if (posX < 0.5 && yaw < M_PI / 12 && !any_bumper_pressed) {
+            angular = 0.0;
+            linear = 0.2;
+        }
+        else if (yaw < M_PI / 2 && posX > 0.5 && !any_bumper_pressed) {
+            angular = M_PI / 6;
+            linear = 0.0;
+        }
+        else {
+            angular = 0.0;
+            linear = 0.0;
+        }
+*/  
+        angular = 1;
+        linear = 0;
+        vel.angular.z = angular;
+        vel.linear.x = linear;
+        vel_pub.publish(vel);
+        
+        std::cout <<"hello" <<std::endl;
+        // The last thing to do is to update the timer.
+        secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
